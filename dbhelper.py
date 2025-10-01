03568f279236476525113e326690e44e520570ea
heyvishusri <vishwaskumar55347@gmail.com>
2025-10-01 21:06:27 +0530

    updated code structure

---
+++ dbhelper.py
@@ -0,0 +1,73 @@
+import mysql.connector as connector
+
+
+class DBHelper:
+    def __init__(self):
+        self.con = connector.connect(
+            host="localhost",
+            port=3306,
+            user="root",
+            password="Vishu9931@@@",
+            database="pythontest",
+        )
+        query = "create table if not exists user(userId int primary key auto_increment,userName varchar(200), phoneNumber varchar(15),email varchar(200), password varchar(20))"
+        cur = self.con.cursor()
+        cur.execute(query)
+        # print("table created")
+
+    # insert data
+    def insert_user(self, userName, phoneNumber, email, password):
+        query = "insert into user(username, phoneNumber, email, password) values('{}','{}','{}','{}')".format(
+            userName, phoneNumber, email, password
+        )
+        print(query)
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        print("user added")
+
+    # fetch data
+    def fetch_all(self):
+        query = "select * from user"
+        cur = self.con.cursor()
+        cur.execute(query)
+        for row in cur:
+            print("UserId:", row[0])
+            print("UserName:", row[1])
+            print("PhoneNumber:", row[2])
+            print("Email:", row[3])
+            print("Password:", row[4])
+            print("====================================")
+            print()
+
+    # fetch user by id
+    def fetch_user_by_id(self, userId):
+        query = "select * from user where userId={}".format(userId)
+        cur = self.con.cursor()
+        cur.execute(query)
+        for row in cur:
+            print("🥳User found!")
+            print()
+            print("UserId:", row[0])
+            print("UserName:", row[1])
+            print("PhoneNumber:", row[2])
+            print("Email:", row[3])
+            print("Password:", row[4])
+            print("====================================")
+            print()
+
+    # delete user by id
+    def delete_user_by_id(self, userId):
+        query = "delete from user where userId={}".format(userId)
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        print("user deleted successfully🥺")
+
+    # update records
+    def update_user_by_id(self, userId, newName):
+        query = "update user set username='{}' where userId={}".format(newName, userId)
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        print("user updated successfully😎")
--- main.py
+++ main.py
@@ -1,76 +1,47 @@
-import mysql.connector as connector
+from dbhelper import DBHelper
 
 
-class DBHelper:
-    def __init__(self):
-        self.con = connector.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="Vishu9931@@@",
-            database="pythontest",
-        )
-        query = "create table if not exists user(userId int primary key auto_increment,userName varchar(200), phoneNumber varchar(15),email varchar(200), password varchar(20))"
-        cur = self.con.cursor()
-        cur.execute(query)
-        # print("table created")
+def main():
+    while True:
+        print("💻WELCOME TO USER MANAGEMENT SYSTEM💻")
+        print("====================================")
+        print()
+        print("PRESS 1 TO INSERT USER")
+        print("PRESS 2 TO FETCH ALL USERS")
+        print("PRESS 3 TO FETCH USER BY ID")
+        print("PRESS 4 TO DELETE USER BY ID")
+        print("PRESS 5 TO UPDATE USER BY ID")
+        print("PRESS 6 TO EXIT")
+        print()
+        try:
+            choice = int(input("ENTER YOUR CHOICE: "))
+            if choice == 1:
+                # insert user
+                pass
+            elif choice == 2:
+                # fetch all users
+                pass
+            elif choice == 3:
+                # fetch user by id
+                pass
+            elif choice == 4:
+                # delete user by id
+                pass
+            elif choice == 5:
+                # update user by id
+                pass
+            elif choice == 6:
+                print("Exiting...👋")
+                break
+            else:
+                print("Invalid choice! Please try again.")
+        except Exception as e:
+            print("Error:", e)
+            print("Invalid input! Please enter a number.")
 
-    # insert data
-    def insert_user(self, userName, phoneNumber, email, password):
-        query = "insert into user(username, phoneNumber, email, password) values('{}','{}','{}','{}')".format(
-            userName, phoneNumber, email, password
-        )
-        print(query)
-        cur = self.con.cursor()
-        cur.execute(query)
-        self.con.commit()
-        print("user added")
 
-    # fetch data
-    def fetch_all(self):
-        query = "select * from user"
-        cur = self.con.cursor()
-        cur.execute(query)
-        for row in cur:
-            print("UserId:", row[0])
-            print("UserName:", row[1])
-            print("PhoneNumber:", row[2])
-            print("Email:", row[3])
-            print("Password:", row[4])
-            print("====================================")
-            print()
-
-    # fetch user by id
-    def fetch_user_by_id(self, userId):
-        query = "select * from user where userId={}".format(userId)
-        cur = self.con.cursor()
-        cur.execute(query)
-        for row in cur:
-            print("🥳User found!")
-            print()
-            print("UserId:", row[0])
-            print("UserName:", row[1])
-            print("PhoneNumber:", row[2])
-            print("Email:", row[3])
-            print("Password:", row[4])
-            print("====================================")
-            print()
-
-    # delete user by id
-    def delete_user_by_id(self, userId):
-        query = "delete from user where userId={}".format(userId)
-        cur = self.con.cursor()
-        cur.execute(query)
-        self.con.commit()
-        print("user deleted successfully🥺")
-
-    # update records
-    def update_user_by_id(self, userId, newName):
-        query = "update user set username='{}' where userId={}".format(newName, userId)
-        cur = self.con.cursor()
-        cur.execute(query)
-        self.con.commit()
-        print("user updated successfully😎")
+if __name__ == "__main__":
+    main()
 
 
 # main code
